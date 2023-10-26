931fe99598801c71f1249652246fe630184ad4a4
ashruti nayak <ashruti.nayak@tatvasoft.com>
2023-10-26 18:56:41 +0530

    first comment

--- src/app/module/home/home.component.ts
+++ src/app/module/home/home.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+type FormType = {
+  lastName: FormControl<string|null>,
+  firstName: FormControl<string|null>,
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
   count = 0;
+  submitFormData:boolean = false
+  submitForm = new FormGroup<FormType>({
+    firstName: new FormControl<string>('', Validators.required),
+    lastName: new FormControl<string>('', Validators.required)
+  });
+  get ctrl(): FormType { return this.submitForm.controls; }
   submit = () => {
-    this.count = this.count + 1;
+    this.submitFormData = true;
+    this.submitForm.markAllAsTouched();
+    if (this.submitForm.valid) {
+      this.count = this.count + 1;
+    }
   }
 }
